f738faf0583b27df9ec743382aa0eace94c580ec
Anuvrat Karanwal <anuvratkaranwal@Apples-MacBook-Pro-10.local>
2022-05-30 16:05:50 +0530

    added tests and logic

--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -1,8 +1,35 @@
 import { dummyOutput } from '../../src/Dummy'
+import { primeFactor } from '../../src/Dummy'
 
 describe("Dummy Test", () => {
     it("should assert on the dummy output", () => {
-        const dummyOutputReturned = dummyOutput("hello")
-        expect(dummyOutputReturned).toBe("hellohello")
+        const dummyOutputReturned = dummyOutput("hello");
+        expect(dummyOutputReturned).toBe("hellohello");
     })
+
+    it("prime factor of 2 should be 2", () => {
+        expect(primeFactor(2)).toEqual([2]);
+    })
+
+    it("prime factor of 3 should be 3", () => {
+        expect(primeFactor(3)).toEqual([3]);
+    })
+
+    it("prime factor of 4 should be 2,2", () => {
+        expect(primeFactor(4)).toEqual([2,2]);
+    })
+
+    it("prime factor of 8 should be 2,2,2", () => {
+        expect(primeFactor(8)).toEqual([2,2,2]);
+    })
+
+    it("prime factor of 25 should be 5,5", () => {
+        expect(primeFactor(25)).toEqual([5,5]);
+    })
+
+    it("prime factor of 100 should be 5,5,2,2", () => {
+        expect(primeFactor(100)).toEqual([2,2,5,5]);
+    })
+        
 })
+
